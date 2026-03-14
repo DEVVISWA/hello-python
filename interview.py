c1a4cf7bbc2224fded2bb22c905e150c2efe51de
Deva dharshini <dev613viswa2gmail.com>
2026-03-14 16:04:53 +0530

    added interview program

---
+++ interview.py
@@ -0,0 +1,54 @@
+#number = int(input("Enter the number:"))
+#if number % 2==0 :
+ # print ("even")
+#else :
+  # print ("Odd")
+
+
+
+#number = [10,5,20,8]
+#large = number[0]
+#for num in number :
+#    if num > large:
+#        large = num 
+ #       print ("large :", large)
+
+
+
+
+#text = input ("enter the text:")
+#count = {}
+#for char in text :
+ #   if char in count :
+  #      count [char] += 1
+   # else :
+    #    count [char]= 1
+
+
+#for char in count :
+ #   print (char ,":",count[char])
+
+
+
+
+#nums=[1,2,3,2,4,5,1]
+#seen = set()
+#duplicate =set ()
+#for num in nums:
+#    if num in seen:
+#        duplicate.add(num)
+#    else :
+#        seen.add(num)
+#for d in duplicate:
+#    print (d)      
+
+
+
+
+
+nums = [5,10,15]
+total= 0
+for num in nums:
+    total = total+num 
+print("sum: ",total)
+    
